72120a0f50a3ff6c9d717286402151251ff15236
Ron Bernshtein <ronbernshtein277@gmail.com>
2024-09-23 14:07:21 +0300

    RootCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponentComponent } from './components/main/login-component/login-component.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent,LoginComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
